00194bd5cda4b29e0e95021d91864bbdb3c7c5ac
satou-yosiyuki <stuysyk1027@icloud.com>
2022-09-17 23:58:42 +0900

    twittercardの型付け

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from "next";
+import { TwitterCard } from "../components/TwitterCard";
+
+// 型推論 型を推測してくれる
+// 型アノテーション 型を明示的に示す
+// 型アサーション型を上書きする
+// プリミティブ型とオブジェクト(プリミティブ型以外、配列、関数など)
+// nullよりundifinedが推奨
+// APIでjsonを返すときはnull
+// LiteralTypes 文字列やNumberやbooleanなどを限定的に宣言する
+//any時は実行するときも何でも行ける
+// unknownは実行するときは型安全
+// void関数の返り値がない
 
 const Home: NextPage = () => {
-  return <div>あいうえお</div>;
+  return (
+    <TwitterCard
+      user={{
+        name: "あいうえお",
+        accountName: "aiueo",
+        image: "https://aiueo",
+      }}
+      body={{
+        text: "おはようございます",
+      }}
+      analytics={[1, 2, 3]}
+    />
+  );
 };
-
 export default Home;
